756fade66274532bb1a1bf36e6748e7cdd18b660
Tudor Garbulet <garbuletvt@ornl.gov>
2024-02-15 14:28:20 -0500

    Adds vitPress deploy to GitHub Pages

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "ARM Data Workbench",
+  description: "Documentation page for ARM Data Workbench",
+  base: '/adw/docs',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      // { text: 'Examples', link: '/get-started' }
+    ],
+
+    sidebar: [
+      // {
+      //   text: 'Getting Started',
+      //   items: [
+      //     { text: 'Requirements', link: '/get-started' }
+      //   ]
+      // },
+      {
+        text: 'Architecture',
+        items: [
+          { text: 'Overview', link: '/architecture' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/ARM-Development/ARM-Data-Workbench' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "ARM Data Workbench"
+  # text: "ADW"
+  tagline: "The supercomputer platform for ARM data"
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /get-started
+    - theme: alt
+      text: Tutorials
+      # link: /tutorials
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
